aea786d7851b27fd8253cf60f899ad09fb32650e
atulkumar65 <45595596+atulkumar65@users.noreply.github.com>
2022-04-01 10:58:32 +0530

    Update card-list.component.jsx

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,7 +5,7 @@ const CardList = ({ monsters }) => {
   return (
     <div className="card-list">
       {monsters.map((monster) => {
-        return <Card key={monster.id} monster={monster} />;
+        return <Card key={monster.id} monster={monster} />;  //generating unique Card for each monster
       })}
     </div>
   );
